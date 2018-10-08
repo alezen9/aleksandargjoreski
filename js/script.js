c103cac30c1e83fdeb84896814cf1704cb65d20c
alezen9 <aj0715@live.com>
2018-10-08 21:29:33 +0200

    speed improvements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// deferr javascript
+function parseJSAtOnload() {
+  var links = ["js/AnimateScroll.min.js", "js/alexBox.min.js","js/carousel.min.js"],
+  headElement = document.getElementsByTagName("head")[0],
+  linkElement, i;
+  for (i = 0; i < links.length; i++) {
+  linkElement = document.createElement("script");
+  linkElement.src = links[i];
+  headElement.appendChild(linkElement);
+  }
+  }
+  if (window.addEventListener)
+  window.addEventListener("load", parseJSAtOnload, false);
+  else if (window.attachEvent)
+  window.attachEvent("onload", parseJSAtOnload);
+  else window.onload = parseJSAtOnload;
+
+  //for smoothscroll
 var anchors = document.querySelectorAll('a');
 
 anchors.forEach(element => {
